f84fbfdf8bbd078e74b766097b2f2337d527e3d4
MARTI Valentin <valentin.marti@hotmail.fr>
2022-06-01 11:38:30 +0200

    changement du visuel de la carte

--- TP_03/script.js
+++ TP_03/script.js
@@ -1,10 +1,9 @@
 let lat = 0;
 let long = 0;
 let map = L.map('map').setView([lat, long], 2);
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 20
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 
